b0bfa1b09d8ae7c4f9b589131e79a3a75b99fe0a
m0bar3z <mzali9895@gmail.com>
2026-01-05 22:38:19 +0330

    feat: app proviers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import AppProviders from "@/providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,10 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AppProviders>{children}</AppProviders>
       </body>
     </html>
   );
---
+++ src/providers.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode } from "react";
+import { WagmiProvider } from "wagmi";
+
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { arbitrum, base, mainnet, optimism, polygon, sepolia } from "wagmi/chains";
+
+import { baseAccount, injectedWallet, rainbowWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
+
+export const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+  chains: [mainnet, sepolia, polygon, optimism, arbitrum, base],
+  wallets: [
+    {
+      groupName: "Recommended",
+      wallets: [injectedWallet, rainbowWallet, walletConnectWallet, baseAccount],
+    },
+  ],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default Providers;
